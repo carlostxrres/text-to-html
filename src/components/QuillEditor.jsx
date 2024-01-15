8dfe9b3120e12384d610834195e5849c188373ae
Carlos <108987011+carlostxrres@users.noreply.github.com>
2024-01-15 10:17:51 +0100

    remove not-working formatting options

--- src/components/QuillEditor.jsx
+++ src/components/QuillEditor.jsx
@@ -32,20 +32,16 @@ export function QuillEditor() {
     toolbar: [
       [{ header: [1, 2, 3, false] }], // 4, 5, 6
       ["bold", "italic", "underline", "strike"],
-      ["blockquote", 'code-block'],
+      ["blockquote"], // 'code-block'
       [{ list: "ordered" }, { list: "bullet" }],
       ["link", "image"],
-      ["clean"],
-
       // [{ 'script': 'sub'}, { 'script': 'super' }],
       [{ 'indent': '-1'}, { 'indent': '+1' }],
       // [{ 'direction': 'rtl' }],
-    
-      [{ 'size': ['small', false, 'large', 'huge'] }],
-    
-      [{ 'color': [] }, { 'background': [] }],
+      // [{ 'size': ['small', false, 'large', 'huge'] }],
+      // [{ 'color': [] }, { 'background': [] }],
       // [{ 'font': [] }],
-      [{ 'align': [] }],
+      // [{ 'align': [] }],
     
       ['clean']      
     ],
